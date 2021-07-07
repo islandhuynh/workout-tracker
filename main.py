af5ee4220b4517d688f8ec61b0b3a23b1d2097a9
Island <islandhuynh@gmail.com>
2021-07-07 05:34:45 -0400

    Added functionality to post and retrieve nutritional data from nutritionix endpoint

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from config import app_id, app_key
+from requests.api import request
+from config import app_id, app_key, sheetys_endpoint
+import requests
+
+text = "I ran 3 miles and walked 2 miles"
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+headers = {
+  "x-app-id": app_id,
+  "x-app-key": app_key,
+  "Content-Type": "application/json"
+}
+
+request_body = {
+  "query": "I ran 3 miles and walked 2 miles",
+}
+
+response = requests.post(url=nutritionix_endpoint, json=request_body,headers=headers)
+print(response.json())
